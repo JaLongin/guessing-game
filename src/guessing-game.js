5e25e6bba8e2e96627d3b71d4a4f7af94b6f95f7
JaLongin <neznayupochemu1@gmail.com>
2022-08-10 23:02:13 +0300

    feat: implement guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.minP = min + 1;
+        this.maxP = max -1;
+        console.log(min, max);
     }
 
     guess() {
-
+        this.lastGuess = Math.ceil((this.maxP - this.minP) / 2) + this.minP;
+        return this.lastGuess;
     }
 
     lower() {
-
+        this.maxP = this.lastGuess - 1;
     }
 
     greater() {
-
+        this.minP = this.lastGuess + 1;
     }
 }
 
